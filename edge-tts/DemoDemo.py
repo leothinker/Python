31837946d14de7cc4649ad4186a04f4eca2a8ba7
LIU Sicheng <scliu@cpii.hk>
2025-03-10 16:28:40 +0800

    feat: update edge-tts for folder

--- edge-tts/DemoDemo.py
+++ edge-tts/DemoDemo.py
@@ -8,14 +8,8 @@ OUTPUT_FILE = "test.mp3"
 
 
 async def amain() -> None:
-    """Main function"""
     communicate = edge_tts.Communicate(TEXT, VOICE)
     await communicate.save(OUTPUT_FILE)
 
 
-if __name__ == "__main__":
-    loop = asyncio.get_event_loop_policy().get_event_loop()
-    try:
-        loop.run_until_complete(amain())
-    finally:
-        loop.close()
+asyncio.run(amain())
---
+++ edge-tts/PythonTest.py
@@ -0,0 +1,35 @@
+import asyncio
+import os
+import time
+
+import edge_tts
+
+
+async def convert(text, file_name) -> None:
+    start_time = time.time()
+    communicate = edge_tts.Communicate(text, "zh-CN-XiaoxiaoNeural")
+    await communicate.save(f"sounds/{file_name}.mp3")
+    print(f"{file_name}用时：{int(time.time() - start_time)}秒")
+
+
+async def amain():
+    # 创建输出文件夹
+    if not os.path.exists("sounds"):
+        os.makedirs("sounds")
+
+    tasks = []
+    # 遍历输入文件
+    for root, dirs, files in os.walk("texts"):
+        for file in files:
+            # 将文件中的文本读出来
+            with open(os.path.join(root, file), "r", encoding="utf-8") as text_file:
+                text = text_file.read()
+                # 读取文件名
+                file_name, ext = os.path.splitext(file)
+                # 加入任务列表
+                tasks.append(convert(text, file_name))
+    # 等待所有任务完成
+    await asyncio.gather(*tasks)
+
+
+asyncio.run(amain())
